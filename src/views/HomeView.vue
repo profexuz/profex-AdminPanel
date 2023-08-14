1d35782ff739508a149ddcb2b11dd256323f3f9c
AzimJan <azimjonjovliev@gmail.com>
2023-08-14 09:51:52 +0500

    set Icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from "../components/common/Header.vue";
+import Sidebar from "../components/common/Sidebar.vue";
+import Footer from "../components/common/Footer.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <Header></Header>
+  <Sidebar></Sidebar>
+  <div class="p-4 sm:ml-64">
+   <div class="p-4 mt-14">
+     <main>
+     </main>
+     <Footer></Footer>
+   </div>
+  </div> 
 </template>
