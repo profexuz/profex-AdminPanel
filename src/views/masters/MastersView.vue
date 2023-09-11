c9cced321643236e016fdbdcbbe26c5ac14cab63
Mirfayz <muiilhom@gmail.com>
2023-09-11 22:16:09 +0500

    Added Master Search by name

--- src/views/masters/MastersView.vue
+++ src/views/masters/MastersView.vue
@@ -31,6 +31,27 @@ export default defineComponent({
 
 
             console.error("Error while fetching data:");
+        },
+
+        async searchDataAsync(){
+            if (this.searchString.trim() === '') {
+                    await this.getDataAsync();
+                    return;
+                }
+            try {
+
+                this.isLoaded = false;
+
+                const response = await axios.get("/api/common/master/search?search="+ this.searchString);
+                
+                this.mastersList = response.data;
+            } 
+            catch(error) {
+                console.log(error)
+            }
+            finally{
+                this.isLoaded = true;
+            }
         }
         
     },
@@ -42,7 +63,8 @@ export default defineComponent({
             page: 1 as number,
             defaultSkeletons: 6 as Number,
             currentPage: 1 as Number,
-            totalPages: 1 as Number
+            totalPages: 1 as Number,
+            searchString: ""
         }
     },
     setup(){
@@ -84,8 +106,26 @@ export default defineComponent({
         </ol>
 
     <!--end:: BreadCrumb-->
-<MasterCreateModal></MasterCreateModal>
 
+    <form class="flex mx-5 justify-end pb-5 w-full" @submit.prevent="searchDataAsync">
+        <div class="flex relative right-0 w-2/5">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input v-model="searchString" type="search" id="default-search"
+            class="flex end w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Name..." />
+          <button type="submit"
+            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {{ $t("search") }}
+          </button>
+        </div>
+    </form>
+    <MasterCreateModal></MasterCreateModal>
 
     </nav>
 
