d4545b69fa802b898afa73474a34ecf9f1667d46
Mirfayz <muiilhom@gmail.com>
2023-09-12 09:49:03 +0500

    Edit

--- src/components/users/UserCreateModal.vue
+++ src/components/users/UserCreateModal.vue
@@ -97,8 +97,8 @@
 
 <!--    button   start   -->
     <button @click="OpenRegister"
-        type="button" class="text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-1 ">
-        <div class="flex flex-wrap items-center">
+        type="button" class="text-white w-32 h-14 bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+        <div class="flex flex">
             <IconCreate></IconCreate>
             <p class="mx-2">{{ $t("create") }}</p>
         </div>
