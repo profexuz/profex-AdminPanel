eaa14f888b13416b6c9661e52699a12145023a09
Mirfayz <muiilhom@gmail.com>
2023-09-12 09:29:54 +0500

    Added PostImage.vue

--- src/views/users/UsersView.vue
+++ src/views/users/UsersView.vue
@@ -35,6 +35,32 @@ export default defineComponent({
             console.log(response.data);
         },
 
+async searchDataAsync(){
+    if (this.searchString.trim() === '') {
+            await this.getDataAsync(1);
+            return;
+        }
+    try {
+
+        this.isLoaded = false;
+
+        const response = await axios.get("/api/common/user/search?search="+ this.searchString);
+        const paginationJson = JSON.parse(response.headers['x-pagination']);
+        this.metaData = new PaginationMetaData();
+        this.metaData.currentPage = paginationJson.CurrentPage;
+        this.metaData.totalPages = paginationJson.TotalPages;
+        this.metaData.hasNext = paginationJson.HasNext;
+        this.metaData.hasPrevious = paginationJson.HasPrevious;
+        this.userlist = response.data;
+    } 
+    catch(error) {
+        console.log(error)
+    }
+    finally{
+        this.isLoaded = true;
+    }
+}
+
     },
     data() {
         return {
@@ -43,6 +69,7 @@ export default defineComponent({
             isLoaded: false as Boolean,
             metaData: new PaginationMetaData(),
             page: 1 as number,
+            searchString: ""
         }
     },
     setup(){
@@ -85,6 +112,25 @@ export default defineComponent({
             </li>
         </ol>
         <!--end:: BreadCrumb-->
+
+        <form class="flex mx-5 justify-end pb-5 w-full" @submit.prevent="searchDataAsync">
+        <div class="flex relative right-0 w-2/5">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input v-model="searchString" type="search" id="default-search"
+            class="flex end w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Name..." />
+          <button type="submit"
+            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {{ $t("search") }}
+          </button>
+        </div>
+        </form>
 
         <UserCreateModal></UserCreateModal>
 
